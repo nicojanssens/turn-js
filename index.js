ab5dc26464cc8afc34487a9b8eb23b3d929b5094
Nico Janssens <nico.nj.janssens@alcatel-lucent.com>
2016-02-29 05:07:39 -0500

    jetlag coding: initial README efforts + exposing stun transports via turn turn lib

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var Attributes = require('./src/attributes')
 var ChannelData = require('./src/channel_data')
 var Packet = require('./src/packet')
+var transports = require('stun-js').transports
 var TurnClient = require('./src/turn_client')
 
 module.exports = function createClient (address, port, user, pwd, transport) {
@@ -14,3 +15,5 @@ module.exports.Attributes = Attributes
 module.exports.ChannelData = ChannelData
 module.exports.Packet = Packet
 module.exports.TurnClient = TurnClient
+// STUN transports
+module.exports.transports = transports
